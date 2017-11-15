692a92fd83a36d9187f48fb4bf70e2760d5ec845
Jay G <playerjayho@gmail.com>
2017-11-15 16:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ function makeInt(n) {
   return parseInt('n', 10)
 }
 
-function preserveDecimal(n) {
-  return parseInt('n', 10)
-}
+
 
 function preserveDecimal(n) {
   return parseFloat('n', 10)
